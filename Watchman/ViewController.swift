9bae8422d4f9c61942a5d2cc19ee0526a7c705e6
Danielle Lancashire <dani@builds.terrible.systems>
2015-09-10 19:29:06 +0100

    Initial Commit

---
+++ Watchman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watchman
+//
+//  Created by  Danielle Lancashireon 10/09/2015.
+//  Copyright © 2015 Rocket Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
